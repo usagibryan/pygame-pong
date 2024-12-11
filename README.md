63ca1b39f72485cdc8ac002c01a1cbdce6c5491d
Frankie <34449029+usagibryan@users.noreply.github.com>
2024-12-10 19:17:28 -0500

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # pygame-pong
-Pong created in Pygame following a tutorial by ClearCode
+Pong created in Pygame following this tutorial by ClearCode: [Learning Pygame by making Pong](https://youtu.be/Qf3-aDXG8q4?si=0dptHxLTtrBgVExI)
--- main.py
+++ main.py
@@ -8,7 +8,11 @@ clock = pygame.time.Clock()
 
 # Setting up the main window
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+crt = CRT(screen)
 pygame.display.set_caption('Pong')
+
+# Game Rectangles
+ball = pygame.Rect(SCREEN_HEIGHT/2 - 15,SCREEN_HEIGHT/2 -15,30,30,)
 
 while True:
     # Handling input
@@ -18,5 +22,6 @@ while True:
             sys.exit()
 
     # Updating the window
+    crt.draw()
     pygame.display.flip()
     clock.tick(FRAMERATE)
