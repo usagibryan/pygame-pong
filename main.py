6116b06be50623e19e1fdedee070df9d32eee16b
Frankie <34449029+usagibryan@users.noreply.github.com>
2024-12-10 19:33:31 -0500

    Add animations

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ crt = CRT(screen)
 pygame.display.set_caption('Pong')
 
 # Game Rectangles
-ball = pygame.Rect(SCREEN_HEIGHT/2 - 15,SCREEN_HEIGHT/2 -15,30,30,)
+ball = pygame.Rect(SCREEN_WIDTH/2 - 15,SCREEN_HEIGHT/2 - 15,30,30)
+player = pygame.Rect(SCREEN_WIDTH - 20, SCREEN_HEIGHT/2 - 70,10,140)
+opponent = pygame.Rect(10, SCREEN_HEIGHT/2 - 70,10,140)
 
 while True:
     # Handling input
@@ -20,6 +22,22 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    ball.x += BALL_SPEED_X
+    ball.y += BALL_SPEED_Y
+
+    if ball.top <= 0 or ball.bottom >= SCREEN_HEIGHT:
+        BALL_SPEED_Y *= -1
+    if ball.left <= 0 or ball.right >= SCREEN_WIDTH:
+        BALL_SPEED_X *= -1
+
+    # Visuals
+    screen.fill(BG_COLOR)
+    pygame.draw.rect(screen,LIGHT_GREY,player)
+    pygame.draw.rect(screen,LIGHT_GREY,opponent)
+    pygame.draw.ellipse(screen,LIGHT_GREY,ball)
+    pygame.draw.aaline(screen,LIGHT_GREY,(SCREEN_WIDTH/2,0),(SCREEN_WIDTH/2,SCREEN_HEIGHT))
+
 
     # Updating the window
     crt.draw()
--- settings.py
+++ settings.py
@@ -3,3 +3,12 @@ import pygame
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 960
 FRAMERATE = 60
+
+# these change as the ball bounces at the edges of the screen
+# so should they be in settings.py if they are not constants?
+BALL_SPEED_X = 7
+BALL_SPEED_Y = 7
+
+# Colors
+BG_COLOR = pygame.Color('grey12')
+LIGHT_GREY = (200,200,200)
